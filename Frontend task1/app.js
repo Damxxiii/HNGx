596a1332ff2d3e54edeeaa982afccb62612b3fc1
dominic <callmedom23@gmail.com>
2023-09-11 15:51:54 +0100

    new

--- Frontend task1/app.js
+++ Frontend task1/app.js
@@ -1,21 +1,3 @@
-const util = require("util");
-const date = new Date();
-const currentDate = date.getUTCDay();
-const days = [
-"Sunday",
-"Monday",
-"Tuesday",
-"Wednesday",
-"Thursday",
-"Friday",
-"Saturday"];
-const currentDay = days[currentDate];
 
-document.getElementById("day").innerHTML = '<b> Day Of The Week:</b> ${currentDay}';
-
-function milliseconds() {
-    const date = new Date();
-    const currentTime = date.getTime();
-    document.getElementById("time").innerHTML = '<b> UTC Time:</b> ${currentTime}'   
-}
-setInterval(milliseconds, 1000);
+var dt = new Date();
+document.getElementById('date-time').innerHTML=dt;
